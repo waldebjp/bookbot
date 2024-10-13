4c85efd6729e0197576b093f719fa6703619694d
waldebjp <waldebjp@hotmail.com>
2024-10-13 14:45:53 -0400

    Added word counting function and character counting function that prints a dictionary of each letter with its respective count

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the text")
+    characters_dict = get_num_characters(text)
+    print(f"The list of characters in the text is:  {characters_dict}")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+characters_dict = {}
+
+def get_num_characters(text):
+    characters_dict = {}
+    for letter in text:
+        lowered = letter.lower()
+        if lowered in characters_dict:
+            characters_dict[lowered] += 1
+        else:
+            characters_dict[lowered] = 1
+    return characters_dict
+
+
+main()
